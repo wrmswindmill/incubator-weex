3f91de0e11a1b6a62011a7473a5b1a70fee9921a
renmin <wrmwindmill@gmail.com>
2019-07-26 23:45:04 +0800

    new dangerfile

--- danger-output.js
+++
@@ -1,5 +0,0 @@
-import { schedule, danger, fail, warn, message, markdown } from "danger";
-var command = 'cat ios/sdk/oclint.log | grep -i "P[1|2]"'
-import shell from "shelljs";
-var output = shell.exec(command).output;
-warn(output);
--- ios/sdk/WeexSDK/Sources/Utility/WXVersion.m
+++ ios/sdk/WeexSDK/Sources/Utility/WXVersion.m
@@ -20,8 +20,8 @@
 #import "WXVersion.h"
 #import "WXDefine.h"
 
-static const char* WeexSDKBuildTime = "2019-07-26 13:35:28 UTC";
-static const unsigned long WeexSDKBuildTimestamp = 1564148128;
+static const char* WeexSDKBuildTime = "2019-07-26 14:47:29 UTC";
+static const unsigned long WeexSDKBuildTimestamp = 1564152449;
 
 NSString* GetWeexSDKVersion(void)
 {
